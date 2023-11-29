20fb745023a3f64653824f00c0cad67ff6fcb79e
Robin Jünemann <robin@juenemanns.de>
2023-11-29 19:34:13 +0100

    added routing node to main

--- landingPage/src/app/app.module.ts
+++ landingPage/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainPageComponent
+    MainPageComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
